8f20d5928801b7ea8d7d0f7b0581549ebed2a194
shota <shalania1263@gmail.com>
2023-08-13 19:17:54 +0400

    Table-Employee first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import EmployeeProvider from "./Context/EmployeeContext";
+import TableHeader from "./Components/Header/tableHeader";
+import TableMain from "./Components/Main/tableMain";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <EmployeeProvider>
+        <EmployeeProvider>
+          <TableHeader />
+          <TableMain />
+        </EmployeeProvider>
+      </EmployeeProvider>
+    </>
   );
 }
 
